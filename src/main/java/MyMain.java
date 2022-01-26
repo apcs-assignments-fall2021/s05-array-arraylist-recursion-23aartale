6de09609c6aea01505453a4107b9303b86f6f840
adrianartale <23aartale@athenian.org>
2022-01-26 13:39:10 -0800

    finished :)

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -235,20 +235,21 @@ public class MyMain {
         else if (mat[row][col] == '*') {
             return false;
         }
-        else if (mat[row][col] == ' '){
-            // Leave "breadcrumbs"
+
+        else if (mat[row][col] == 'f'){
+            return true;
+        }
+        else{
             mat[row][col] = '*';
 
             // Visit our neighbors (left, up, right, down)
-            floodFill(mat, row, col-1);
-            floodFill(mat, row-1, col);
-            floodFill(mat, row, col+1);
-            floodFill(mat, row+1, col);
-            }
+            boolean b1 = escape(mat, row, col-1);
+            boolean b2 = escape(mat, row-1, col);
+            boolean b3 = escape(mat, row, col+1);
+            boolean b4 = escape(mat, row+1, col);
 
-        else{
-            return true;
-        }
+            return (b1 || b2 || b3 || b4);
+            }
     }
 
 
